41cf59c0ba06a9dffa9b0e09f8dda05467ae88db
scotty1373 <spsim1373@outlook.com>
2022-04-02 19:20:30 +0800

    bug fixed

--- mmdet/core/hook/msg_hook.py
+++ mmdet/core/hook/msg_hook.py
@@ -7,29 +7,36 @@ import torch
 from mmcv.runner import Hook, HOOKS
 import requests
 
+
+def msg_send(name, ep, log_dict):
+    content_str = f"Epoch={ep}, " \
+                  f"loss_rpn_cls={log_dict['loss_rpn_cls'][-1]:.4f}, " \
+                  f"loss_rpn_bbox={log_dict['loss_rpn_bbox'][-1]:.4f}, " \
+                  f"loss_roi_cls={log_dict['loss_cls'][-1]:.4f}, " \
+                  f"loss_roi_bbox={log_dict['loss_bbox'][-1]:.4f}, " \
+                  f"acc={log_dict['acc'][-1]}, " \
+                  f"total_loss={log_dict['loss'][-1]:.4f}"
+    resp = requests.get(
+        "https://www.autodl.com/api/v1/wechat/message/push?token={token}&title={title}&name={name}&content={content}".format(
+            token="e571d1889d78",
+            title="Wechat MSG",
+            name=name,
+            content=content_str))
+    print(resp.content.decode())
+
+
 @HOOKS.register_module()
 class SendMsgInfoHook(Hook):
+    def __init__(self, send=True):
+        self.send = send
+
     """Send message to WeChat Terminal"""
-    def after_train_epoch(self, runner):
-        epoch = runner.epochs
+    def after_train_iter(self, runner):
+        epoch = runner.epoch
         model_name = runner.model_name
         log_dict = runner.log_buffer.val_history
-        if epoch % 4:
-            self.msg_send(model_name, epoch, log_dict)
+        if not epoch % 4:
+            msg_send(model_name, epoch, log_dict)
 
-    @staticmethod
-    def msg_send(name, ep, log_dict):
-        content_str = f"Epoch={ep}, " \
-                      f"loss_rpn_cls={log_dict['loss_rpn_cls']}, " \
-                      f"loss_rpn_bbox={log_dict['loss_rpn_bbox']}, " \
-                      f"loss_roi_cls={log_dict['loss_cls']}, " \
-                      f"loss_roi_bbox={log_dict['loss_bbox']}, " \
-                      f"acc={log_dict['acc']}, " \
-                      f"total_loss={log_dict['loss']}"
-        resp = requests.get("https://www.autodl.com/api/v1/wechat/message/push?token={token}&title={title}&name={name}&content={content}".format(
-                token="e571d1889d78",
-                title="Wechat MSG",
-                name=name,
-                content=content_str))
-        print(resp.content.decode())
+
 

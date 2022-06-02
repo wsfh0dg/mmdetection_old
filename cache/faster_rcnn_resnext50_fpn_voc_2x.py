2251998f79032a962900af5ff0804b4a28b03d80
scotty1373 <1024935841@qq.com>
2022-06-02 14:46:56 +0800

    提交dior修改

--- cache/faster_rcnn_resnext50_fpn_voc_2x.py
+++ cache/faster_rcnn_resnext50_fpn_voc_2x.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
-fp16 = dict(loss_scale=512.)
 model = dict(
     type='FasterRCNN',
     backbone=dict(
-        type='ResNet',
+        type='ResNeXt',
         depth=50,
         num_stages=4,
+        groups=32,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        init_cfg=dict(
+            type='Pretrained', checkpoint='torchvision://resnext50_32x4d')),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -28,7 +28,7 @@ model = dict(
             strides=[4, 8, 16, 32, 64]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
-            target_means=[.0, .0, .0, .0],
+            target_means=[0.0, 0.0, 0.0, 0.0],
             target_stds=[1.0, 1.0, 1.0, 1.0]),
         loss_cls=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
@@ -48,13 +48,12 @@ model = dict(
             num_classes=20,
             bbox_coder=dict(
                 type='DeltaXYWHBBoxCoder',
-                target_means=[0., 0., 0., 0.],
+                target_means=[0.0, 0.0, 0.0, 0.0],
                 target_stds=[0.1, 0.1, 0.2, 0.2]),
             reg_class_agnostic=False,
             loss_cls=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
             loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
-    # model training and testing settings
     train_cfg=dict(
         rpn=dict(
             assigner=dict(
@@ -88,7 +87,7 @@ model = dict(
                 ignore_iof_thr=-1),
             sampler=dict(
                 type='RandomSampler',
-                num=256,
+                num=128,
                 pos_fraction=0.25,
                 neg_pos_ub=-1,
                 add_gt_as_proposals=True),
@@ -103,96 +102,132 @@ model = dict(
         rcnn=dict(
             score_thr=0.05,
             nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=100)
-        # soft-nms is also supported for rcnn testing
-        # e.g., nms=dict(type='soft_nms', iou_threshold=0.5, min_score=0.05)
-    ))
-
-
-# dataset settings
-dataset_type = 'DIORDataset'
-data_root = 'DIOR/'
+            max_per_img=100)))
+dataset_type = 'VOCDataset'
+data_root = 'VOC/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1000, 1000), keep_ratio=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1000, 1000),
+        img_scale=(1000, 600),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
+            dict(type='Collect', keys=['img'])
         ])
 ]
 data = dict(
-    samples_per_gpu=3,
-    workers_per_gpu=3,
+    samples_per_gpu=6,
+    workers_per_gpu=6,
     train=dict(
         type='RepeatDataset',
-        times=2,
+        times=3,
         dataset=dict(
-            type=dataset_type,
-            ann_file=[data_root + 'dior_datasets/' + 'ImageSets/Main/train.txt'],
-            img_prefix=[data_root + 'dior_datasets'],
-            pipeline=train_pipeline)),
+            type='VOCDataset',
+            ann_file=['VOC/VOC2007/ImageSets/Main/trainval.txt'],
+            img_prefix=['VOC/VOC2007/'],
+            pipeline=[
+                dict(type='LoadImageFromFile'),
+                dict(type='LoadAnnotations', with_bbox=True),
+                dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+                dict(type='RandomFlip', flip_ratio=0.5),
+                dict(
+                    type='Normalize',
+                    mean=[123.675, 116.28, 103.53],
+                    std=[58.395, 57.12, 57.375],
+                    to_rgb=True),
+                dict(type='Pad', size_divisor=32),
+                dict(type='DefaultFormatBundle'),
+                dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+            ])),
     val=dict(
-        type=dataset_type,
-        ann_file=data_root + 'dior_datasets/' + 'ImageSets/Main/val.txt',
-        img_prefix=data_root + 'dior_datasets',
-        pipeline=test_pipeline),
+        type='VOCDataset',
+        ann_file='VOC/VOC2007/ImageSets/Main/test.txt',
+        img_prefix='VOC/VOC2007/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1000, 600),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]),
     test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'dior_test/' + 'ImageSets/Main/test.txt',
-        img_prefix=data_root + 'dior_test',
-        pipeline=test_pipeline))
+        type='VOCDataset',
+        ann_file='VOC/VOC2007/ImageSets/Main/test.txt',
+        img_prefix='VOC/VOC2007/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1000, 600),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]))
 evaluation = dict(interval=1, metric='mAP')
-
-# runtime config
 checkpoint_config = dict(interval=1)
-# yapf:disable
-log_config = dict(
-    interval=50,    # interval日志保存间隔
-    hooks=[
-        dict(type='TextLoggerHook'),
-        dict(type='TensorboardLoggerHook')
-    ])
-# yapf:enable
-custom_hooks = [dict(type='NumClassCheckHook'),
-                dict(type='SendMsgInfoHook')]
-
+log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
+custom_hooks = [dict(type='NumClassCheckHook'), dict(type='SendMsgInfoHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
-
-# optimizer config
-# schedules 2x
 optimizer = dict(type='SGD', lr=0.00125, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
-# learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[12, 22])
+    step=[16, 22])
 runner = dict(type='EpochBasedRunner', max_epochs=24)
-
-# work_dir = 'cache/'
-
+work_dir = 'cache/'
+auto_resume = False
+gpu_ids = [0]
